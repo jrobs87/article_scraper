49799dd42b1a74dca7c29859dded087f48d518e4
John Robertson <tiktaalikdesign@gmail.com>
2019-07-03 23:32:05 -0400

    updated views, routes

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose')
 const cheerio = require('cheerio')
 const axios = require('axios')
 
-
 const app = express()
 var port = process.env.PORT || 3000;
 
@@ -22,8 +21,34 @@ app.engine("handlebars",
 app.set("view engine", "handlebars");
  
 // Routes
+// HTML - Home
 app.get('/', function (req, res) {
     res.render('home');
+});
+
+// HTML - Saved
+app.get('/saved', function (req, res) {
+  res.render('saved');
+});
+
+// API - Scrape
+app.get('/api/scrape', function (req, res) {
+  res.send('Scrape Articles Route').status(200).end();
+});
+
+// API - Clear
+app.get('/api/clear', function (req, res) {
+  res.send('Clear Articles Route').status(200).end();
+});
+
+// API - Add Comment
+app.get('/saved', function (req, res) {
+  res.render('saved');
+});
+
+// API - Delete Comment
+app.get('/saved', function (req, res) {
+  res.render('saved');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
