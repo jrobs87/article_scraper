15ff1888642d5221c6634dd1f74d53a6fad2d3f3
John Robertson <tiktaalikdesign@gmail.com>
2019-07-10 22:02:51 -0400

    models set

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const exphbs  = require('express-handlebars')
 const mongoose = require('mongoose')
-const cheerio = require('cheerio')
-const axios = require('axios')
+const db = require('./models');
+const cheerio = require('cheerio');
+const axios = require('axios');
 
 const app = express()
 var port = process.env.PORT || 3000;
@@ -13,6 +14,8 @@ app.use(express.static('public'))
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
+
+
 
 // Handlebars
 app.engine("handlebars",
